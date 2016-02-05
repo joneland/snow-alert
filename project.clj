e9c188e5b26709d7dee398537b57d9ada7166e78
joneland <jonathan.eland@gmail.com>
2016-02-05 13:55:28 +0000

    Added conjure stubbing library

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :source-paths ["src/main/clj" ]
   :test-paths [ "src/test/clj" ]
   :dependencies
-  [[org.clojure/clojure "1.8.0"]])
+  [[org.clojure/clojure "1.8.0"]
+   [org.clojars.runa/conjure "2.1.3"]])
--- src/test/clj/snow_alert/resources_test.clj
+++ src/test/clj/snow_alert/resources_test.clj
@@ -1,6 +1,14 @@
 (ns snow-alert.resources-test
+  (:use conjure.core)
   (:require [clojure.test :refer :all]
             [snow-alert.resources :as r]))
 
+(defn add [x y]
+  )
+
+(defn fn-under-test [x y]
+  (add x y))
+
 (deftest one-plus-one-equals-snow? 
-  (is (= "snow" (+ 1 1))))
+  (stubbing [add "snow"]
+    (is (= "snow" (fn-under-test 1 1)))))
